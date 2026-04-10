660eb3f1055d30b6beacd9b898e959e40d366021
bulbel-magnolia <1878614882@qq.com>
2026-04-10 17:08:39 +0800

    PM2 内存上限从 512M 提高到 2G

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,7 +6,8 @@ module.exports = {
     instances: 1,
     autorestart: true,
     watch: false,
-    max_memory_restart: "512M",
+    max_memory_restart: "2G",
+    kill_timeout: 10000,
     env: {
       NODE_ENV: "production",
       PORT: 3000
